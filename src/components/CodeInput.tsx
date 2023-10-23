ddf22fa88c1b283d32f1bdcfd625026380d67d58
Drew Harris <harrisd@smu.edu>
2023-10-22 23:07:36 -0500

    Change placeholder

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -6,7 +6,7 @@ export default function CodeInput() {
         hx-post="/render"
         hx-target="#result"
         hx-trigger="keyup changed delay:500ms"
-        placeholder="Enter flowscript here"
+        placeholder="Enter flowscript here..."
         class="p-2 border-black w-full border-2"
       ></textarea>
     </div>
