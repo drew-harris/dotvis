646b9b3ed00ad0da8c76381dd82cf8633440b338
Drew Harris <harrisd@smu.edu>
2023-10-22 23:43:27 -0500

    Add indicator

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -1,14 +1,24 @@
 export default function CodeInput() {
   return (
     <div class="">
-      <textarea
-        name="code"
-        hx-post="/render"
-        hx-target="#result"
-        hx-trigger="keyup changed delay:500ms"
-        placeholder="Enter flowscript here..."
-        class="p-2 border-black w-full border-2"
-      ></textarea>
+      <pre class="relative">
+        <textarea
+          name="code"
+          hx-post="/render"
+          hx-target="#result"
+          hx-trigger="keyup changed delay:500ms"
+          hx-indicator=".htmx-indicator"
+          rows="20"
+          placeholder="Enter flowscript here..."
+          class="relative p-2 border-black w-full border-2"
+        ></textarea>
+        <img
+          src="/spinner"
+          width="28"
+          height="28"
+          class="htmx-indicator absolute top-2 right-2"
+        />
+      </pre>
     </div>
   );
 }
--- src/components/mainpage.tsx
+++ src/components/mainpage.tsx
@@ -3,10 +3,12 @@ import CodeInput from "./CodeInput";
 export default function MainPage() {
   return (
     <div>
-      <div>Flowscript Language Visualizer</div>
-      <CodeInput />
-      <div id="result">
-        <div id="error"></div>
+      <div class="text-lg font-bold">Flowscript Language Visualizer</div>
+      <div class="grid grid-cols-2 gap-2">
+        <CodeInput />
+        <div id="result">
+          <div id="error"></div>
+        </div>
       </div>
     </div>
   );
